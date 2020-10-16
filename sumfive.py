92b42387296dfc9266ddc8720e634227f5731138
Albin Thankachan <31209982+albin22@users.noreply.github.com>
2020-10-16 23:20:07 +0530

    Create sumfive.py

---
+++ sumfive.py
@@ -0,0 +1,8 @@
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+c = int(input("enter second number: "))
+d = int(input("enter second number: "))
+e = int(input("enter second number: "))
+sum = a + b+c+d+e
+
+print("sum:", sum)
